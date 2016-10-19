21ca139687a440cfadf6a7775b6e7b3b63fbab0a
stableShip <296888680@qq.com>
2016-10-19 15:54:33 +0800

    console message count

--- consumer.js
+++ consumer.js
@@ -10,10 +10,13 @@ co(function* () {
 
         yield channel1.assertQueue(q);
 
+        var num = 0;
         channel1.consume(q, function (msg) {
             if (msg !== null) {
                 console.log("get Message: ", msg.content.toString());
                 channel1.ack(msg);
+                num++;
+                console.log("total: ", num);
             }
         });
     } catch (err) {
